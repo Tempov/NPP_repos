c5703f7d1203e209d034552e850f19f40cd861b2
John Smith <mr.tempov@gmail.com>
2021-11-23 21:29:30 +0200

    test 05

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {User} from "./user";
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(
+      {
+          type: 'mysql',
+          host: '127.0.0.1',
+          port: 3306,
+          username: 'Tempov',
+          password: '222333',
+          database: 'base_npp',
+          entities: [User],
+          synchronize: true,
+      }),],
   controllers: [AppController],
   providers: [AppService],
 })
